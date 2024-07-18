722ade31014f8862bb83c1311a4da845bca6fe8d
Rafael Freitas <oraffaudi@gmail.com>
2024-07-18 09:46:21 -0300

    Messages Component - Step-by-Step Implementation

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -14,4 +14,12 @@ import {NgClass} from "@angular/common";
 export class MessagesComponent {
 
 
+  messagesService = inject(MessagesService);
+
+  message = this.messagesService.message;
+
+  onClose(){
+    this.messagesService.clear();
+  }
+
 }
